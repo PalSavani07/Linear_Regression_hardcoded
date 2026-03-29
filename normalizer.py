27b29c562f7bbc09ed4422b59ce2a45d2fbf705a
PalSavani07 <palsavani90@gmail.com>
2026-03-29 11:33:52 +0530

    Scales the Data

--- normalizer.py
+++ normalizer.py
@@ -9,12 +9,60 @@ def get_min_max(data):
 
     for i in range(1,len(data)):
         for j in range(n_cols):
-            if data[i][j]<min[j]:
+            if data[i][j]<mins[j]:
                  mins[j] = data[i][j]
             if data[i][j] > maxs[j]:
                 maxs[j] = data[i][j]
     
     return mins,maxs
 
+
 def normalize(data,mins,maxs):
+    if len(mins) != len(maxs):
+        raise ValueError("[normalize] mins and maxs must have the same length.")
+ 
+    if len(data[0]) != len(mins):
+        raise ValueError(
+            f"[normalize] data has {len(data[0])} columns "
+            f"but mins/maxs have {len(mins)} values."
+        )
+ 
+    scaled = []
+ 
+    for i in range(len(data)):
+        scaled_row = []
+        for j in range(len(data[i])):
+            range_val = maxs[j] - mins[j]
+ 
+            if range_val == 0:
+                # All values in this column are the same → scale to 0
+                scaled_row.append(0.0)
+            else:
+                scaled_row.append((data[i][j] - mins[j]) / range_val)
+ 
+        scaled.append(scaled_row)
+ 
+    return scaled
+
+
+def normalize_1d(data,mins,maxs):
+    """
+    Applies Min-Max normalization to a 1D list (e.g. Y values).
+    """
+
+    range_val=maxs-mins
+
+    if range_val==0:
+        raise ValueError("[normalize_1d] max_val == min_val, cannot normalize.")
     
+    scaled=[]
+    for val in data:
+        scaled.append((val-mins)/range_val)
+
+    return scaled
+
+def denormalize(scaled_val,mins,maxs):
+
+    return (scaled_val*(maxs-mins))+mins
+
+
